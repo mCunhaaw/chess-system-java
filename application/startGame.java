5425da20fd4cec177dc4f3d3d147ab0cc23ef5ed
mCunhaaw <cunhaa.mcss@gmail.com>
2023-05-26 23:11:19 -0300

    startGame criado, como tela de início + algumas correções no código

--- application/startGame.java
+++ application/startGame.java
@@ -1,5 +1,32 @@
 package application;
+import java.util.Scanner;
 
 public class startGame {
+    public static void main(String[] args) {
+        exibirTelaInicial();
+        iniciarJogo();
+    }
 
+    public static void exibirTelaInicial() {
+    	Scanner sc = new Scanner(System.in);
+        String opcao = sc.nextLine();
+        
+        System.out.println("===== JOGO DE XADREZ =====");
+        System.out.println("Bem-vindo(a) ao jogo de xadrez!");
+        System.out.println("Clique em Start para iniciar o jogo.");
+        System.out.println("==========================");
+        System.out.print("Opção: ");
+
+        if (opcao.equalsIgnoreCase("start")) {
+            System.out.println("Iniciando o jogo...");
+        } else {
+            System.out.println("Opção inválida. O jogo será encerrado.");
+            System.exit(0);
+        }
+        sc.close();
+    }
+
+    public static void iniciarJogo() {
+        
+    }
 }
